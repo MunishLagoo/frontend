e1a6bbc488887bd49371c4197f3312e91ca21da3
Munish Lagoo <munishlagoo@gmail.com>
2021-12-23 12:23:57 -0500

    modify frontend vars.tf

--- terraform-mutable/vars.tf
+++ terraform-mutable/vars.tf
@@ -2,12 +2,6 @@ variable "ENV" {}
 variable "OD_INSTANCE_COUNT" {}
 variable "SPOT_INSTANCE_COUNT" {}
 variable "INSTANCE_TYPE" {}
-variable "APP_VERSION" {
-    default = ""
-}
-variable "NEXUS_USER" {
-    default = ""
-}
-variable "NEXUS_PASS" {
-    default = ""
-}
+variable "APP_VERSION" {}
+variable "NEXUS_USER" {}
+variable "NEXUS_PASS" {}
